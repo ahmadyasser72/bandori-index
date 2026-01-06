fee740c44df7a1d6065476918b7c6ae63e51e642
ahmadyasser72 <163807415+ahmadyasser72@users.noreply.github.com>
2026-01-06 08:40:46 +0800

    first commit

---
+++ prettier.config.js
@@ -0,0 +1,28 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+	trailingComma: "all",
+	semi: true,
+	singleQuote: false,
+	useTabs: true,
+
+	plugins: [
+		"prettier-plugin-astro",
+		"@ianvs/prettier-plugin-sort-imports",
+		"prettier-plugin-tailwindcss",
+	],
+	overrides: [{ files: "*.astro", options: { parser: "astro" } }],
+	importOrder: [
+		"^astro",
+		"",
+		"<THIRD_PARTY_MODULES>",
+		"",
+		"^@bandori-index/(.*)$",
+		"^~/(.*)$",
+		"^[./]",
+	],
+};
+
+export default config;
